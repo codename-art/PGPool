f4e31ef728b69ec21a9cabb9e665cc00dfac2ba6
sLoPPydrive <sloppydrive@gmail.com>
2017-05-26 14:54:06 +0200

    Use an alternative config file via commandline -c, —config <filename>

--- pgscout/config.py
+++ pgscout/config.py
@@ -2,6 +2,8 @@ import json
 import os
 
 # Configuration with default values
+import configargparse
+
 cfg = {
     'host': '127.0.0.1',
     'port': 4242,
@@ -16,7 +18,12 @@ def cfg_get(key, default=None):
     return cfg.get(key, default)
 
 
-file_path = os.path.join('config.json')
-with open(file_path, 'r') as f:
+parser = configargparse.ArgParser()
+parser.add_argument('-c', '--config',
+                    help=('Specify different config file. Default: config.json'),
+                    default='config.json')
+args = parser.parse_args()
+
+with open(args.config, 'r') as f:
     user_cfg = json.loads(f.read())
     cfg.update(user_cfg)
