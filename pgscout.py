e97e2e834ee316d44b70b2abb13d089907bff477
sLoPPydrive <sloppydrive@gmail.com>
2017-05-31 12:47:33 +0200

    Allow for numeric spawn_point_id (like Monocle uses)

--- pgscout.py
+++ pgscout.py
@@ -11,7 +11,8 @@ from pgscout.cache import get_cached_encounter, cache_encounter, cleanup_cache
 from pgscout.config import cfg_get
 from pgscout.console import print_status
 from pgscout.proxy import init_proxies
-from pgscout.utils import get_pokemon_name, normalize_encounter_id
+from pgscout.utils import get_pokemon_name, normalize_encounter_id, \
+    normalize_spawn_point_id
 
 logging.basicConfig(level=logging.INFO,
     format='%(asctime)s [%(threadName)16s][%(module)14s][%(levelname)8s] %(message)s')
@@ -38,7 +39,7 @@ def get_iv():
     lng = request.args["longitude"]
 
     encounter_id = normalize_encounter_id(request.args.get("encounter_id"))
-    spawn_point_id = request.args.get("spawn_point_id")
+    spawn_point_id = normalize_spawn_point_id(request.args.get("spawn_point_id"))
 
     # Check cache
     cache_key = encounter_id if encounter_id else "{}-{}-{}".format(pokemon_id, lat, lng)
